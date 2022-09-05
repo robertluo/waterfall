607e638306dc40fc469e48167629b08bec210649
Luo Tian <lt@basecity.com>
2022-09-05 08:40:37 -0700

    improve definition->config, introduce EventSender

--- src/waterfall/core.clj
+++ src/waterfall/core.clj
@@ -4,48 +4,70 @@
    [malli.core :as m])
   (:import
    (java.util Map)
-   (org.apache.kafka.clients.producer KafkaProducer ProducerConfig)))
+   (org.apache.kafka.clients.producer KafkaProducer ProducerConfig Producer)))
 
 ;; Kafka producer/consumer need config map.
 ;; Instead of using properties in java API, we introduce a concept of definition.
 
 (def schema-registry
-  {:server/name     string?
-   :server/port     pos-int?
+  {:server/name     :string
+   :server/port     [:and :int pos?]
    ::server         [:map :server/name :server/port]
    :cluster/servers [:+ ::server]
-   :serde/type      [:enum :string :byte-array]
+   :serde/type      [:enum :string :byte-array {:default :byte-array}]
    :key/serde       :serde/type
    :value/serde     :serde/type
-   ::producer       [:map :cluster/servers :key/serde :value/serde]})
+   :topic/name      :string
+   :group/id        :string
+   :producer/compression [:enum :none :gzip :snappy :lz4 :zstd {:default :none}]
+   ::producer       [:map
+                     :cluster/servers
+                     :topic/name
+                     [:key/serde {:optional true}]
+                     [:value/serde {:optional true}]
+                     [:producer/compression {:optional true}]]})
 
 (def default-definition
   {:key/serde :byte-array
    :value/serde :byte-array})
 
-(defn def->cfg
+(defn definition->config
   "returns Kafka config map for `definition`"
   [definition] 
   (letfn [(svr->str [{:server/keys [name port]}] (str name ":" port))
           (serde [k] (k {:string "org.apache.kafka.common.serialization.StringSerializer"
-                         :byte-array "org.apache.kafka.common.serialization.ByteArraySerializer"}))]
-    (let [{servers :cluster/servers key-serde :key/serde val-serde :value/serde}
-          (merge default-definition definition)]
-      {ProducerConfig/BOOTSTRAP_SERVERS_CONFIG
-       (transduce (comp (map svr->str) (interpose ";")) str servers)
-       ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG (serde key-serde)
-       ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG (serde val-serde)})))
+                         :byte-array "org.apache.kafka.common.serialization.ByteArraySerializer"}))
+          (process [[k const f]] (when-let [v (get definition k)] [const (f v)]))]
+    (let [processors [[:cluster/servers ProducerConfig/BOOTSTRAP_SERVERS_CONFIG #(transduce (comp (map svr->str) (interpose ";")) str %)]
+                      [:key/serde ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG serde]
+                      [:value/serde ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG serde]
+                      [:producer/compression ProducerConfig/COMPRESSION_TYPE_CONFIG name]]]
+      (into {} (comp (map process) (filter identity)) processors))))
 
 (comment
-  (def clu {:cluster/servers [#:server{:name "localhost" :port 9092}]
-            :key/serde :byte-array})
+  (def clu {:cluster/servers [#:server{:name "localhost" :port 9092}] 
+            :topic/name "greater"})
   (m/explain [:schema {:registry schema-registry} ::producer] clu)
-  (def->cfg clu)
+  (definition->config clu)
   )
 
+(defprotocol EventSender
+  "A sender can send arbitary clojure data to an event bus (a.k.a Kafka topic)"
+  (-send! [sender event] "send an event"))
+
+(defn event->produce-record
+  [definition event])
+
+(defrecord KafkaEventSender [definition ^Producer producer]
+  EventSender
+  (-send!
+   [_ event]
+   (.send producer (event->produce-record definition event))))
+
 (defn producer
-  [cluster]
-  (KafkaProducer. ^Map (def->cfg cluster)))
+  [definition]
+  (KafkaProducer. ^Map (definition->config definition)))
 
 (comment
   (producer clu))
+  
--- test/waterfall/core_test.clj
+++ test/waterfall/core_test.clj
@@ -3,12 +3,9 @@
    [clojure.test :refer [deftest is testing]]
    [waterfall.core :as sut]))
 
-(deftest def->cfg
+(deftest definition->config
   (testing ":cluster/servers will expand to bootstrap.servers"
-    (is (= {"bootstrap.servers" "localhost:9092;node2:9091"
-            "key.serializer"
-            "org.apache.kafka.common.serialization.ByteArraySerializer",
-            "value.serializer"
-            "org.apache.kafka.common.serialization.ByteArraySerializer"} 
-           (sut/def->cfg {:cluster/servers [#:server{:name "localhost" :port 9092}
-                                            #:server{:name "node2" :port 9091}]})))))
+    (is (= {"bootstrap.servers" "localhost:9092;node2:9091"} 
+           (sut/definition->config {:cluster/servers
+                                    [#:server{:name "localhost" :port 9092}
+                                     #:server{:name "node2" :port 9091}]})))))
