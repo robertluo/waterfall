dc4e686af868a9bdb28a7c910b5d7c11427246d4
Luo Tian <lt@basecity.com>
2022-12-08 12:13:20 +0800

    tune producer

--- src/waterfall/core.clj
+++ src/waterfall/core.clj
@@ -34,6 +34,22 @@
   [cluster-def]
   (KafkaProducer. ^Map (def->config cluster-def) (ByteArraySerializer.) (ByteArraySerializer.)))
 
+(defn- rmd->map
+  [^RecordMetadata rmd]
+  (zipmap [:offset :partition :key-size :value-size :timestamp :topic]
+          [(.offset rmd) (.partition rmd) (.serializedKeySize rmd)
+           (.serializedValueSize rmd) (.timestamp rmd) (.topic rmd)]))
+
+(defn- produce-record?
+  [x]
+  (and (map? x)
+       (let [{:keys [k v topic partition timestamp]} x]
+         (and (not-empty topic) (bytes? k) (bytes? v)
+              (or (nil? partition) (pos-int? partition))
+              (or (nil? timestamp) (pos-int? timestamp))))))
+
+;; Caution: Manifold uses interface for sind/source definition
+;; Modify sink/source might need restart REPL
 (sc/def-sink KafkaProducerSink
   [^Producer producer]
   (isSynchronous [_] false)
@@ -45,12 +61,16 @@
     :closed? (.markClosed this)})
   (put 
    [_ x blocking?]
+   (assert (produce-record? x) (str "invalid produce record: " x))
    (let [{:keys [k v topic partition timestamp]} x
          pr (ProducerRecord. topic partition timestamp k v)    
          rslt (.send producer pr)]
-     (if blocking? @rslt rslt)))
+     (if blocking? 
+       (rmd->map @rslt) 
+       (defer/chain rslt rmd->map))))
   (put
    [this x _ _ _]
+   ;;TODO use future cancel to implement timeout?
    (.put this x false)))
 
 (extend-protocol sc/Sinkable
@@ -59,10 +79,11 @@
     (->KafkaProducerSink producer)))
 
 (comment
-  (def clu {:cluster/servers [#:server{:name "localhost" :port 9092}]}) 
+  (def clu {:cluster/servers [#:server{:name "localhost" :port 9092}]})
   (def->config clu)
   (def a (ms/->sink (producer clu)))
-  (ms/put! a {:topic "test" :k (.getBytes "hello") :v (.getBytes "world")})
+  (ms/put! a {:topic "hello" :k (.getBytes "hello") :v (.getBytes "world")})
+  @*1
   )
 
 (defn source
