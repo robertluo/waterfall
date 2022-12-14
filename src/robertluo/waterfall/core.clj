fac8a8c919c284f55d6673d23940d26c2074d048
Luo Tian <lt@basecity.com>
2022-12-14 16:23:55 +0800

    local refactor

--- src/robertluo/waterfall/core.clj
+++ src/robertluo/waterfall/core.clj
@@ -46,32 +46,53 @@
  [offset partition serializedKeySize serializedValueSize
   timestamp timestampType topic key value headers])
 
-(defn consumer-loop
-  [^Consumer consumer mailbox out-sink]
-  (loop []
-    (when-not 
-     (= ::stop
-        (match @(ms/take! mailbox)
-          [:close] (do (.close consumer) ::stop)
-          [:subscibe topics] (.subscribe consumer topics)
-          [:poll duration]
-          (do (ms/put-all! out-sink (->> (.poll consumer duration) (.iterator) (iterator-seq) (map cr->map)))
-              (ms/put! mailbox [:poll duration]))))
-      (recur))))
+(defn- consumer-actor
+  "Kafka consumer is not thread safe, using actor model will limit its access in a single thread."
+  [^Consumer consumer out-sink]
+  (let [mailbox (ms/stream)
+        cmd-self (fn [cmd] (ms/put! mailbox cmd))]
+    (d/future
+      (loop []
+        (when-not
+         (= ::stop
+            (let [cmd @(ms/take! mailbox)]
+              (match cmd
+               [:close] (do (.close consumer) ::stop)
+               [:subscibe topics] (.subscribe consumer topics)
+               [:seek :beginning] (.seekToBeginning consumer (.assignment consumer))
+               [:seek :end] (.seekToEnd consumer (.assignment consumer))
+               [:resume duration] (do (when (.paused consumer)
+                                        (.resume consumer (.assignment consumer)))
+                                      (.commitSync consumer)
+                                      (cmd-self [:poll duration]))
+               [:poll duration]
+               (let [f-poll #(->> (.poll consumer duration) (.iterator) (iterator-seq) (map cr->map))]
+                 (when-not (.paused consumer)
+                   (.pause consumer (.assignment consumer)))
+                 (d/chain (ms/put-all! out-sink (f-poll))
+                          (fn [rslt]
+                            (when rslt
+                              (cmd-self [:resume duration])))))
+               :else (ex-info "unknown command for consumer actor" {:cmd cmd}))) )
+          (recur))))
+    cmd-self))
 
 (defn consumer
-  [nodes group-id topics {:keys [poll-duration] :as conf :or {poll-duration (Duration/ofSeconds 10)}}]
+  [nodes group-id topics 
+   {:keys [poll-duration position] :as conf 
+    :or {poll-duration (Duration/ofSeconds 10)}}]
   (let [conr (-> conf
                  (merge {:bootstrap-servers nodes
-                         :group-id group-id})
+                         :group-id group-id
+                         :enable-auto-commit false})
                  util/->config-map
-                 (KafkaConsumer. (ByteArrayDeserializer.) (ByteArrayDeserializer.)))
-        mailbox (ms/stream)
-        out-sink (ms/stream)]
-    (ms/put! mailbox [:subscibe topics]) 
-    (ms/on-closed out-sink (fn [] @(ms/put! mailbox [:close])))
-    (d/future (consumer-loop conr mailbox out-sink))
-    (ms/put! mailbox [:poll poll-duration])
+                 (KafkaConsumer. (ByteArrayDeserializer.) (ByteArrayDeserializer.))) 
+        out-sink (ms/stream)
+        actor (consumer-actor conr out-sink)]
+    (actor [:subscibe topics])
+    (when position (actor [:seek position]))
+    (ms/on-closed out-sink (fn [] @(actor [:close]))) 
+    (actor [:poll poll-duration])
     (ms/source-only out-sink)))
 
 (comment
