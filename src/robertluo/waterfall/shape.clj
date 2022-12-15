f208dc7c871b78e3db6152995b807caf6944488c
Luo Tian <lt@basecity.com>
2022-12-16 00:28:59 +0800

    change shape from keyword to function

--- src/robertluo/waterfall/shape.clj
+++ src/robertluo/waterfall/shape.clj
@@ -1,5 +1,6 @@
 (ns robertluo.waterfall.shape
   "Transformation functions of shapes of event data."
+  (:refer-clojure :exclude [byte-array])
   (:require [clojure.edn :as edn]))
 
 (defn- updater
@@ -9,69 +10,80 @@
     (let [f (fn [v] (when v (f v)))]
       (-> m (update :key f) (update :value f)))))
 
-(def default-shapers
-  "default shapers table.
-   A shaper is a map contains 3 keys:
-    - `:ser`: a function to serialize value.
-    - `:des`: a function to deserialize value.
-    - `:stage`: a number indicate the order of the execution."
-  {:value-only 
-   {:doc "Only concerns the value of the Kafka event"
-    :ser (fn [value] {:value value})
-    :des (fn [m] (:value m))
-    :stage 100}
-   :key-value 
-   {:doc "Concerns both the key and value of the Kafka event."
-    :ser (fn [[key value]] {:key key :value value})
-    :des (fn [m] [(:key m) (:value m)])
-    :stage 100}
-   :byte-array
-   {:doc "conversion of byte-array/string."
-    :ser (updater (fn [^String s] (.getBytes s "UTF-8")))
-    :des (updater (fn [^"[B" bs] (String. bs "UTF-8")))
-    :stage 10}
-   :edn
-   {:doc "conversion of string/edn"
-    :ser (updater (fn [data] (pr-str data)))
-    :des (updater (fn [s] (edn/read-string s)))
-    :stage 20}
-   :topic
-   {:doc "high order serialization only, attach topic information"
-    :ser (fn [topic] (fn [m] (assoc m :topic topic)))
-    :stage 0}})
+;;A shape is a conversion definition. It has:
+;; - :stage, a number indicate when should it be called, high = serial early/des late
+;; - :ser, a function for seriliaztion a value.
+;; - :des, a function for deserilization a value.
+(defrecord Shape [stage ser des])
+
+(defn value-only 
+  "A shope only concerns the value of Kafka events."
+  []
+  (->Shape 
+   100
+   (fn [value] {:value value})
+   (fn [m] (:value m))))
+
+(defn key-value 
+  "A shape concerns both the key and value of the Kafka event."
+  []
+  (->Shape
+   100
+   (fn [[key value]] {:key key :value value})
+   (fn [m] [(:key m) (:value m)])))
+
+(defn edn
+  "A shape converts string/edn."
+  []
+  (->Shape
+   20
+   (updater (fn [data] (pr-str data)))
+   (updater (fn [s] (edn/read-string s)))))
+
+(defn byte-array
+  "A shape converts byte-array/string."
+  []
+  (->Shape
+   10
+   (updater (fn [^String s] (.getBytes s "UTF-8")))
+   (updater (fn [^"[B" bs] (String. bs "UTF-8")))))
+
+(defn topic
+  "A shape attach topic to record on serilization, do nothing when deserilizing."
+  [topic-name]
+  (->Shape
+   0
+   (fn [m] (assoc m :topic topic-name))
+   identity))
 
 (defn- fn-selector
   "return functions in a `shapers` data structure in order."
-  [ks shapers f-pair]
-  (->> ks
-       (map #(if (vector? %) % [%])) ;normalize 
-       (map (fn [[k & args]]
-              (let [[order target-f] (some->> (get shapers k) (f-pair))]
-                (if (and order target-f)
-                  [order (if (seq args) (apply target-f args) target-f)]
-                  (throw (ex-info "Invalid shape" {:ks ks})))))) 
-       (sort-by first)
-       (map second)))
+  [shapes f-field]
+  (let [f-pair (juxt :stage f-field)]
+    (->> shapes
+         (map (fn [shape]
+                (let [[order target-f] (f-pair shape)]
+                  (if (and order target-f)
+                    [order target-f]
+                    (throw (ex-info "Invalid shape." {:stage order :f target-f})))))) 
+         (sort-by first)
+         (map second))))
 
 (defn serialize
   "returns a serialization function specified in `ks`"
-  ([ks]
-   (serialize ks default-shapers))
-  ([ks shapers]
-   (->> (fn-selector (set ks) shapers (juxt :stage :ser))
-        (apply comp))))
+  [ks]
+  (->> (fn-selector ks :ser)
+       (apply comp)))
 
 (defn deserialize
   "returns a deserialization function specified in `ks`"
-  ([ks]
-   (deserialize ks default-shapers))
-  ([ks shapers]
-   (->> (fn-selector (set ks) shapers (juxt :stage :des))
-        (reverse)
-        (apply comp))))
+  [ks] 
+  (->> (fn-selector ks :des)
+       (reverse)
+       (apply comp)))
 
 (comment 
-  ((serialize [:byte-array :edn :value-only]) {:string "hello"}) 
-  ((deserialize [:byte-array :value-only :edn]) {:value (.getBytes "3")})
-  ((serialize [:edn :value-only [:topic "test"]]) {:foo "hello"})
+  ((serialize [(byte-array) (edn) (value-only)]) {:string "hello"}) 
+  ((deserialize [(byte-array) (value-only) (edn)]) {:value (.getBytes "3")})
+  ((serialize [(edn) (value-only) (topic "test")]) {:foo "hello"})
   )
--- test/robertluo/waterfall/shape_test.clj
+++ test/robertluo/waterfall/shape_test.clj
@@ -1,28 +1,26 @@
 (ns robertluo.waterfall.shape-test
   (:require
-   [robertluo.waterfall.shape :as sut]
+   [robertluo.waterfall.shape :as s]
    [expectations.clojure.test :refer [defexpect expect]]))
 
 (defexpect serialize
-  (expect {:value "{:foo bar}" :key nil} ((sut/serialize [:value-only :edn]) {:foo 'bar})
+  (expect {:value "{:foo bar}" :key nil} ((s/serialize [(s/value-only) (s/edn)]) {:foo 'bar})
           "serialize value-only and edn")
-  (expect {:key "3" :value "5"} ((sut/serialize [:edn :key-value]) [3 5])
+  (expect {:key "3" :value "5"} ((s/serialize [(s/edn) (s/key-value)]) [3 5])
           "serialize key-value and edn"))
 
 (defexpect deserialize
-  (expect {:foo 'bar} ((sut/deserialize [:value-only :edn]) {:value "{:foo bar}"})
+  (expect {:foo 'bar} ((s/deserialize [(s/value-only) (s/edn)]) {:value "{:foo bar}"})
           "deserilize value-only edn, copied the value/expection")
-  (expect [3 5] ((sut/deserialize [:key-value :edn]) {:key "3" :value "5"})
-          "deserilize :key-value and :edn"))
+  (expect [3 5] ((s/deserialize [(s/key-value) (s/edn)]) {:key "3" :value "5"})
+          "deserilize key-value and edn"))
 
 (defexpect topic-shape
-  (expect {:foo "bar" :topic "test"} ((sut/serialize [[:topic "test"]]) {:foo "bar"})
-          ":topic is a high order shape")
-  (expect clojure.lang.ExceptionInfo ((sut/deserialize [[:topic "test"]]) 'anything)
-          ":topic can not be used on deserialization"))
+  (expect {:foo "bar" :topic "test"} ((s/serialize [(s/topic "test")]) {:foo "bar"})
+          ":topic is a high order shape"))
 
 (defexpect round-trip
-  (let [ks [:value-only :edn :byte-array]
+  (let [ks [(s/value-only) (s/edn) (s/byte-array)]
         data {:foo 'bar :a 3 :b "4"}]
-    (expect data (->> data ((sut/serialize ks)) ((sut/deserialize ks)))
+    (expect data (->> data ((s/serialize ks)) ((s/deserialize ks)))
             "real world example for edn plain value")))
