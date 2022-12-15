e727f8329a0b5671d4f65d8ef5d9b54363772af1
Luo Tian <lt@basecity.com>
2022-12-16 02:21:11 +0800

    test more

--- test/robertluo/waterfall_test.clj
+++ test/robertluo/waterfall_test.clj
@@ -1,6 +1,6 @@
 (ns robertluo.waterfall-test
   (:require [clojure.test :refer [use-fixtures]]
-            [expectations.clojure.test :refer [defexpect expect]]
+            [expectations.clojure.test :refer [defexpect expect more]]
             [robertluo.waterfall :as sut]
             [robertluo.waterfall.shape :as shape]
             [manifold.stream :as ms])
@@ -18,11 +18,14 @@
 (def nodes (str "localhost:" (EmbeddedKafkaConfig/defaultKafkaPort)))
 
 (defexpect round-trip
-  (with-open [test-consumer (-> (sut/consumer nodes "test.group" ["test"])
-                                (sut/shaped-source [(shape/value-only) (shape/edn) (shape/byte-array)]))
-              test-producer (-> (sut/producer nodes)
-                                (sut/ignore)
-                                (sut/shaped-sink [(shape/value-only) (shape/edn) (shape/byte-array) (shape/topic "test")]))]
-    (ms/consume prn test-consumer)
-    (expect true @(ms/put! test-producer "Hello, world!")
-            "Run without exception!")))
+  (let [collector (atom [])]
+    (with-open [test-consumer (-> (sut/consumer nodes "test.group" ["test"])
+                                  (sut/shaped-source [(shape/value-only) (shape/edn) (shape/byte-array)]))
+                test-producer (-> (sut/producer nodes)
+                                  (sut/ignore)
+                                  (sut/shaped-sink [(shape/value-only) (shape/edn) (shape/byte-array) (shape/topic "test")]))]
+      (ms/consume #(do (swap! collector conj %) nil) test-consumer)
+      (expect true @(ms/put-all! test-producer (range 1000))
+              "Run without exception!") 
+      (expect (more not-empty #(every? number? %)) @collector 
+              "Not sure what received, but at least got some numbers."))))
